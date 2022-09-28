a4932a514c7482df055c89dc95f2f076f4eae602
lanntxyz <mr.n.anrew@gmail.com>
2022-09-28 11:59:42 +0000

    add theme for shopxuany

---
+++ shopxuany/admin/language/en-gb/theme/standard.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'Shop Xuan Y Theme';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified the Shop Xuan Y theme!';
+$_['text_edit'] = 'Edit Shop Xuan Y Theme';
+
+// Entry
+$_['entry_status'] = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify the Shop Xuan Y theme!';
